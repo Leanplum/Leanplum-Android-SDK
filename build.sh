e61b980c37785fbb562bba497770c9daa5cdcf0e
Yilong Chang <yilong@leanplum.com>
2018-01-16 15:50:55 -0800

    Copy Javadoc for split SDK modules.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 set -eo pipefail; [[ $DEBUG ]] && set -x
 
 # Check for Jenkins build number, otherwise default to curent time in seconds.
-if [[ -z "${BUILD_NUMBER+x}" ]]; then 
+if [[ -z "${BUILD_NUMBER+x}" ]]; then
   BUILD_NUMBER=$(date "+%s")
 fi
 export ANDROID_VERSION_STRING=${ANDROID_VERSION_STRING:-${ANDROID_VERSION}.${BUILD_NUMBER}}
@@ -26,14 +26,37 @@ default="${LEANPLUM_SDK_ROOT}"
 android_dir=${android_dir:-$default}
 default="${android_dir}/AndroidSDK"
 sdk_dir=${sdk_dir:-$default}
-release_dir="${android_dir}/Release"
+sdk_release_dir="${android_dir}/Release"
 
-rm -rf "$release_dir"
-mkdir -p "$release_dir"
+: ${sdk_core_dir:=$android_dir/AndroidSDKCore}
+sdk_core_release_dir="${sdk_release_dir}/AndroidSDKCoreRelease"
+: ${sdk_fcm_dir:=$android_dir/AndroidSDKFcm}
+sdk_fcm_release_dir="${sdk_release_dir}/AndroidSDKFcmRelease"
+: ${sdk_gcm_dir:=$android_dir/AndroidSDKGcm}
+sdk_gcm_release_dir="${sdk_release_dir}/AndroidSDKGcmRelease"
+: ${sdk_location_dir:=$android_dir/AndroidSDKLocation}
+sdk_location_release_dir="${sdk_release_dir}/AndroidSDKLocationRelease"
+: ${sdk_push_dir:=$android_dir/AndroidSDKPush}
+sdk_push_release_dir="${sdk_release_dir}/AndroidSDKPushRelease"
+
+rm -rf "$sdk_release_dir"
+mkdir -p "$sdk_release_dir"
 
 # Build the AndroidSDK using gradle.
 rm -rf "${sdk_dir}/build"
 rm -rf "${sdk_dir}/javadoc"
+
+rm -rf "${sdk_core_dir}/build"
+rm -rf "${sdk_core_dir}/javadoc"
+rm -rf "${sdk_fcm_dir}/build"
+rm -rf "${sdk_fcm_dir}/javadoc"
+rm -rf "${sdk_gcm_dir}/build"
+rm -rf "${sdk_gcm_dir}/javadoc"
+rm -rf "${sdk_location_dir}/build"
+rm -rf "${sdk_location_dir}/javadoc"
+rm -rf "${sdk_push_dir}/build"
+rm -rf "${sdk_push_dir}/javadoc"
+
 if [[ -z ${upload+x} ]]; then
   GRADLE_TASK="assemble${configuration} makeJar generateJavadoc"
 else
@@ -44,7 +67,18 @@ cd "${android_dir}/Leanplum-Android-SDK"
 # shellcheck disable=SC2086
 ./gradlew $GRADLE_TASK
 
-mv "${sdk_dir}/javadoc" "${release_dir}/."
-cp "${sdk_dir}/build/intermediates/bundles/release/classes.jar" "${release_dir}/Leanplum.jar"
+mv "${sdk_dir}/javadoc" "${sdk_release_dir}/."
+cp "${sdk_dir}/build/intermediates/bundles/release/classes.jar" "${sdk_release_dir}/Leanplum.jar"
+
+mv "${sdk_core_dir}/javadoc" "${sdk_core_release_dir}/."
+cp "${sdk_core_dir}/build/intermediates/bundles/release/classes.jar" "${sdk_core_release_dir}/Leanplum-core.jar"
+mv "${sdk_fcm_dir}/javadoc" "${sdk_fcm_release_dir}/."
+cp "${sdk_fcm_dir}/build/intermediates/bundles/release/classes.jar" "${sdk_fcm_release_dir}/Leanplum-fcm.jar"
+mv "${sdk_gcm_dir}/javadoc" "${sdk_gcm_release_dir}/."
+cp "${sdk_gcm_dir}/build/intermediates/bundles/release/classes.jar" "${sdk_gcm_release_dir}/Leanplum-gcm.jar"
+mv "${sdk_location_dir}/javadoc" "${sdk_location_release_dir}/."
+cp "${sdk_location_dir}/build/intermediates/bundles/release/classes.jar" "${sdk_location_release_dir}/Leanplum-location.jar"
+mv "${sdk_push_dir}/javadoc" "${sdk_push_release_dir}/."
+cp "${sdk_push_dir}/build/intermediates/bundles/release/classes.jar" "${sdk_push_release_dir}/Leanplum-push.jar"
 
 echo "${GREEN} Done.${NORMAL}"
