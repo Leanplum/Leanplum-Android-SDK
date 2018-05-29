3c41b9ec171c357620c001f9f78693afa4228aeb
Mayank Sanganeria <mayank.ot@gmail.com>
2018-05-29 16:46:09 -0700

    update to persist and to have a param

--- AndroidSDKTests/src/test/java/com/leanplum/LeanplumTest.java
+++ AndroidSDKTests/src/test/java/com/leanplum/LeanplumTest.java
@@ -383,7 +383,7 @@ public class LeanplumTest extends AbstractTest {
       }});
     }};
 
-    VarCache.applyVariableDiffs(null, messages, null, null, null, variants);
+    VarCache.applyVariableDiffs(null, messages, null, null, null, variants, null);
     assertEquals(variants, Leanplum.variants());
     assertEquals(messages, Leanplum.messageMetadata());
   }
