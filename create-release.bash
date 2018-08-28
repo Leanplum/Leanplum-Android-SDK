07310aeb3d92d8d1181e1e0bd5f49d918219bcf5
Sayaan Saha <sayaan@leanplum.com>
2018-08-28 14:11:37 -0700

    removed main function from bash script

--- create-release.bash
+++ create-release.bash
@@ -8,14 +8,13 @@ set -o errexit
 #######################################
 # Create a new release branch
 #######################################
-main() {
-  TYPE=$1
+TYPE=$1
 
-  release_version=$(./Tools/create-release.py ${TYPE})
+release_version=$(./Tools/create-release.py ${TYPE})
 
-  # create a branch, push on success
-  git checkout -b "release/${release_version}"
-  git push --set-upstream origin "release/${release_version}"
-}
+# create a branch, push on success
+# git checkout -b "release/${release_version}"
+# git push --set-upstream origin "release/${release_version}"
 
-main "$@"
+
+
