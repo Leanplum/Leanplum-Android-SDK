072238cb67322443d6070d9bf49e0cd8dd3a0596
Kelly Johnson <MaitreyaBuddha@users.noreply.github.com>
2018-09-06 13:11:24 -0700

    Fix release script to have everything we need (#240)
    Actually add our version file, safely make the commit and push the branch

--- Tools/create-release.bash
+++ Tools/create-release.bash
@@ -11,7 +11,12 @@ set -o errexit
 TYPE=$1
 
 release_version=$(./Tools/create-release.py ${TYPE})
+cat AndroidSDKCore/sdk-version.txt
 
-# create a branch, push on success
-git checkout -b "release/${release_version}"
-git push --set-upstream origin "release/${release_version}"
+git reset HEAD --
+RELEASE_BRANCH=release/${release_version}
+git checkout -b ${RELEASE_BRANCH}
+
+git add AndroidSDKCore/sdk-version.txt
+git commit -m "Create ${TYPE} release ${release_version}"
+git push --set-upstream origin ${RELEASE_BRANCH}
