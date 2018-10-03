9d0d4e6029fcb265f3ffe90ed7574cf263e45fc6
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2018-10-02 20:30:42 -0700

    add request id to param dictionary

--- AndroidSDKCore/src/main/java/com/leanplum/internal/Request.java
+++ AndroidSDKCore/src/main/java/com/leanplum/internal/Request.java
@@ -253,6 +253,7 @@ public class Request {
     args.put(Constants.Params.SDK_VERSION, Constants.LEANPLUM_VERSION);
     args.put(Constants.Params.DEV_MODE, Boolean.toString(Constants.isDevelopmentModeEnabled));
     args.put(Constants.Params.TIME, Double.toString(new Date().getTime() / 1000.0));
+    args.put(Constants.Params.REQ_ID, Integer.toString(requestId));
     if (token != null) {
       args.put(Constants.Params.TOKEN, token);
     }
