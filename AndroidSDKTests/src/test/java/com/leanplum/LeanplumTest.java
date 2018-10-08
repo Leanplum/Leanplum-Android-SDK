379d74d0c0ffb9dece9bc8c04ed896652899af21
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2018-10-08 10:46:07 -0700

    remove unused helper methods

--- AndroidSDKTests/src/test/java/com/leanplum/LeanplumTest.java
+++ AndroidSDKTests/src/test/java/com/leanplum/LeanplumTest.java
@@ -1427,32 +1427,4 @@ public class LeanplumTest extends AbstractTest {
     }
     return responsesList;
   }
-
-  private boolean JSONObjectsAreEqual(Object ob1, Object ob2) throws JSONException {
-    Object obj1Converted = convertJsonElement(ob1);
-    Object obj2Converted = convertJsonElement(ob2);
-    return obj1Converted.equals(obj2Converted);
-  }
-
-  private Object convertJsonElement(Object elem) throws JSONException {
-    if (elem instanceof JSONObject) {
-      JSONObject obj = (JSONObject) elem;
-      Iterator<String> keys = obj.keys();
-      Map<String, Object> jsonMap = new HashMap<>();
-      while (keys.hasNext()) {
-        String key = keys.next();
-        jsonMap.put(key, convertJsonElement(obj.get(key)));
-      }
-      return jsonMap;
-    } else if (elem instanceof JSONArray) {
-      JSONArray arr = (JSONArray) elem;
-      Set<Object> jsonSet = new HashSet<>();
-      for (int i = 0; i < arr.length(); i++) {
-        jsonSet.add(convertJsonElement(arr.get(i)));
-      }
-      return jsonSet;
-    } else {
-      return elem;
-    }
-  }
 }
