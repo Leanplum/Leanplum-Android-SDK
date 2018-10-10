cb516fd7a6667530ae09ccf653381d1297c23e0f
Grace Gu <grace.gu@leanplum.com>
2018-10-10 16:05:18 -0700

    remove twonot needed imports

--- AndroidSDKCore/src/main/java/com/leanplum/Leanplum.java
+++ AndroidSDKCore/src/main/java/com/leanplum/Leanplum.java
@@ -58,7 +58,6 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
