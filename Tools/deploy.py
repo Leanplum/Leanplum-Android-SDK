e3b0e080f60323d68ca9e24ce26ce86fd16c2628
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2018-10-09 17:56:17 -0700

    deploy script

--- Tools/deploy.py
+++ Tools/deploy.py
@@ -12,36 +12,37 @@ def deploy(project, package, version):
 
 def deployAAR(project, package, version):
     localAARPath = project + "/build/outputs/aar/" + project + "-release.aar"
-    filename = remoteFilename(package, version, "aar")
     
-    remoteAARPath = "libs-release-local/com/leanplum/" + package + "/" + version + "/" + filename
+    remoteAARPath = "libs-release-local/" + remotePath(package, version, "aar")
     artifactoryDeploy(localAARPath, remoteAARPath)
     
-    remoteBintrayPath = "https://api.bintray.com/content/leanplum/maven/" + package + "/" + version + "/" + filename
+    remoteBintrayPath = "https://api.bintray.com/content/leanplum/maven/" + package + "/" + remotePath(package, version, "aar")
     bintrayDeploy(localAARPath, remoteBintrayPath)
 
 def deployPOM(project, package, version):
     localAARPath = project + "/build/publications/aar/pom-default.xml"
-    filename = remoteFilename(package, version, "pom")
 
-    remoteAARPath = "libs-release-local/com/leanplum/" + package + "/" + version + "/" + package + "-" + version + ".pom"
+    remoteAARPath = "libs-release-local/" + remotePath(package, version, "pom")
     artifactoryDeploy(localAARPath, remoteAARPath)
     
-    remoteBintrayPath = "https://api.bintray.com/content/leanplum/maven/" + package + "/" + version + "/" + filename
+    remoteBintrayPath = "https://api.bintray.com/content/leanplum/maven/" + package + "/" + remotePath(package, version, "pom")
     bintrayDeploy(localAARPath, remoteBintrayPath)
-    
+
+def remotePath(package, version, extension):
+    path = "com/leanplum/" + package + "/" + version + "/" + remoteFilename(package, version, extension)
+    return path
 
 def remoteFilename(package, version, extension):
     return package + "-" + version + "." + extension
 
 def artifactoryDeploy(source, destination):
     command = "jfrog rt u " + source + " " + destination
-    print command
-    # os.system(command)
+    # print command
+    os.system(command)
 
 def bintrayDeploy(source, destination):
-    command = "curl -T " + source + " -ue7mac:<API_KEY> " + destination
-    print command
+    command = "curl -T " + source + " -ue7mac:" + "API_KEY " + destination
+    # print command
     # os.system(command)
     
 
