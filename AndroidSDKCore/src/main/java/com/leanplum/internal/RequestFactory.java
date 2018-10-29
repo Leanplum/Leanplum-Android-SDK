505fbd9385966b37ba2158fe564be8a025e80658
Grace Gu <grace.gu@leanplum.com>
2018-10-29 16:10:41 -0700

    add some methods, change some to private

--- AndroidSDKCore/src/main/java/com/leanplum/internal/RequestFactory.java
+++ AndroidSDKCore/src/main/java/com/leanplum/internal/RequestFactory.java
@@ -44,7 +44,7 @@ public class RequestFactory {
   private static final String API_METHOD_SET_DEVICE_ATTRIBUTES = "setDeviceAttributes";
   private static final String API_METHOD_SET_TRAFFIC_SOURCE_INFO = "setTrafficSourceInfo";
   private static final String API_METHOD_UPLOAD_FILE = "uploadFile";
-  private static final String PI_METHOD_DOWNLOAD_FILE = "downloadFile";
+  private static final String API_METHOD_DOWNLOAD_FILE = "downloadFile";
   private static final String API_METHOD_HEARTBEAT = "heartbeat";
   private static final String API_METHOD_SAVE_VIEW_CONTROLLER_VERSION = "saveInterface";
   private static final String API_METHOD_SAVE_VIEW_CONTROLLER_IMAGE = "saveInterfaceImage";
@@ -69,14 +69,119 @@ public class RequestFactory {
     return new RequestOld(httpMethod, apiMethod, params);
   }
 
-  public Requesting createGetForApiMethod(String apiMethod, Map<String, Object> params) {
+  public Requesting startWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_START, params);
+  }
+
+  public Requesting getVarsWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_GET_VARS, params);
+  }
+
+  public Requesting setVarsWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_SET_VARS, params);
+  }
+
+  public Requesting stopWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_STOP, params);
+  }
+
+  public Requesting restartWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_RESTART, params);
+  }
+
+    public Requesting trackWithParams(Map<String, Object> params) {
+      return createPostForApiMethod(API_METHOD_TRACK, params);
+  }
+
+  public Requesting advanceWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_ADVANCE, params);
+  }
+
+  public Requesting pauseSessionWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_PAUSE_SESSION, params);
+  }
+
+  public Requesting pauseStateWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_PAUSE_STATE, params);
+  }
+
+  public Requesting resumeSessionWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_RESUME_SESSION, params);
+  }
+
+  public Requesting resumeStateWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_RESUME_STATE, params);
+  }
+
+  public Requesting multiWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_MULTI, params);
+  }
+
+  public Requesting registerDeviceWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_REGISTER_FOR_DEVELOPMENT, params);
+  }
+
+  public Requesting setUserAttributesWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_SET_USER_ATTRIBUTES, params);
+  }
+
+  public Requesting setDeviceAttributesWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_SET_DEVICE_ATTRIBUTES, params);
+  }
+
+  public Requesting setTrafficSourceInfoWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_SET_TRAFFIC_SOURCE_INFO, params);
+  }
+
+  public Requesting uploadFileWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_UPLOAD_FILE, params);
+  }
+
+  public Requesting downloadFileWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_DOWNLOAD_FILE, params);
+  }
+
+  public Requesting heartbeatWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_HEARTBEAT, params);
+  }
+
+  public Requesting saveInterfaceWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_SAVE_VIEW_CONTROLLER_VERSION, params);
+  }
+
+  public Requesting saveInterfaceImageWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_SAVE_VIEW_CONTROLLER_IMAGE, params);
+  }
+
+  public Requesting getViewControllerVersionsListWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_GET_VIEW_CONTROLLER_VERSIONS_LIST, params);
+  }
+
+  public Requesting logWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_LOG, params);
+  }
+
+  public Requesting getNewsfeedMessagesWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_GET_INBOX_MESSAGES, params);
+  }
+
+  public Requesting markNewsfeedMessageAsReadWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_MARK_INBOX_MESSAGE_AS_READ, params);
+  }
+
+  public Requesting deleteNewsfeedMessagesWithParams(Map<String, Object> params) {
+    return createPostForApiMethod(API_METHOD_DELETE_INBOX_MESSAGE, params);
+  }
+
+
+  private Requesting createGetForApiMethod(String apiMethod, Map<String, Object> params) {
     if (shouldReturnLPRequestClass()) {
       return Request.get(apiMethod, params);
     }
     return RequestOld.get(apiMethod, params);
   }
 
-  public Requesting createPostForApiMethod(String apiMethod, Map<String, Object> params) {
+  private Requesting createPostForApiMethod(String apiMethod, Map<String, Object> params) {
     if (shouldReturnLPRequestClass()) {
       return Request.post(apiMethod, params);
     }
