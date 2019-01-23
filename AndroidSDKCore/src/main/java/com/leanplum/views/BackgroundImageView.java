bc1afd05a9f1c4831593a1d2bb7cf34e3eb0ba65
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-01-23 12:07:00 -0800

    remove old class

--- AndroidSDKCore/src/main/java/com/leanplum/views/BackgroundImageView.java
+++ AndroidSDKCore/src/main/java/com/leanplum/views/BackgroundImageView.java
@@ -29,7 +29,6 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.PorterDuff.Mode;
-import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.RelativeLayout.LayoutParams;
