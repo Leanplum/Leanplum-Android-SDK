a72fbe8db4282d27ff339d4c054dc64cc6602b3a
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-03-25 11:55:56 -0700

    no need to store in sharedprefs

--- AndroidSDKCore/src/main/java/com/leanplum/internal/RequestOldUtil.java
+++ AndroidSDKCore/src/main/java/com/leanplum/internal/RequestOldUtil.java
@@ -1,28 +1,15 @@
 package com.leanplum.internal;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-
-import com.leanplum.Leanplum;
-import com.leanplum.utils.SharedPreferencesUtil;
-
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import static com.leanplum.internal.RequestOld.LEANPLUM;
 import static com.leanplum.internal.RequestOld.UUID_KEY;
 
 public class RequestOldUtil {
 
     public void setNewBatchUUID(List<Map<String, Object>> requests) {
         String uuid = UUID.randomUUID().toString();
-        Context context = Leanplum.getContext();
-        SharedPreferences preferences = context.getSharedPreferences(
-                LEANPLUM, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(Constants.Defaults.UUID_KEY, uuid);
-        SharedPreferencesUtil.commitChanges(editor);
         for (Map<String, Object> request : requests) {
             request.put(UUID_KEY, uuid);
         }
