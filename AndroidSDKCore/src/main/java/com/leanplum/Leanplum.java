276ac75061867208a13d9cdd8d13a4d47f88fc88
Hristo Borisov <hristo.borisov@leanplum.com>
2021-11-22 17:18:00 +0200

    Implement Huawei push messages (#476)

--- AndroidSDKCore/src/main/java/com/leanplum/Leanplum.java
+++ AndroidSDKCore/src/main/java/com/leanplum/Leanplum.java
@@ -674,6 +674,8 @@ public class Leanplum {
         Constants.Defaults.LEANPLUM_PUSH, Constants.Defaults.PROPERTY_FCM_TOKEN_ID);
     String miPushRegistrationId = SharedPreferencesUtil.getString(context,
         Constants.Defaults.LEANPLUM_PUSH, Constants.Defaults.PROPERTY_MIPUSH_TOKEN_ID);
+    String hmsRegistrationId = SharedPreferencesUtil.getString(context,
+        Constants.Defaults.LEANPLUM_PUSH, Constants.Defaults.PROPERTY_HMS_TOKEN_ID);
 
     HashMap<String, Object> params = new HashMap<>();
     params.put(Constants.Params.INCLUDE_DEFAULTS, Boolean.toString(false));
@@ -690,6 +692,9 @@ public class Leanplum {
     }
     if (!TextUtils.isEmpty(miPushRegistrationId)) {
       params.put(Constants.Params.DEVICE_MIPUSH_TOKEN, miPushRegistrationId);
+    }
+    if (!TextUtils.isEmpty(hmsRegistrationId)) {
+      params.put(Constants.Params.DEVICE_HMS_TOKEN, hmsRegistrationId);
     }
     params.put(Constants.Keys.TIMEZONE, localTimeZone.getID());
     params.put(Constants.Keys.TIMEZONE_OFFSET_SECONDS, Integer.toString(timezoneOffsetSeconds));
@@ -1551,6 +1556,9 @@ public class Leanplum {
         break;
       case MIPUSH:
         attributeName = Constants.Params.DEVICE_MIPUSH_TOKEN;
+        break;
+      case HMS:
+        attributeName = Constants.Params.DEVICE_HMS_TOKEN;
         break;
       default:
         return;
---
+++ AndroidSDKHms/src/main/java/com/leanplum/LeanplumHmsProvider.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2021, Leanplum, Inc. All rights reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.leanplum;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.widget.Toast;
+import com.huawei.agconnect.AGConnectOptionsBuilder;
+import com.huawei.agconnect.config.AGConnectServicesConfig;
+import com.huawei.hmf.tasks.OnCompleteListener;
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.aaid.HmsInstanceId;
+import com.huawei.hms.aaid.entity.AAIDResult;
+import com.huawei.hms.push.HmsMessaging;
+import com.huawei.hms.utils.Util;
+import com.leanplum.internal.Constants.Defaults;
+import com.leanplum.internal.Log;
+
+/**
+ * Leanplum provider for Huawei Push Kit.
+ * Class is instantiated by reflection using default constructor.
+ */
+class LeanplumHmsProvider extends LeanplumCloudMessagingProvider {
+
+  /**
+   * Constructor called by reflection.
+   */
+  public LeanplumHmsProvider() {
+  }
+
+  @Override
+  protected String getSharedPrefsPropertyName() {
+    return Defaults.PROPERTY_HMS_TOKEN_ID;
+  }
+
+  @Override
+  public PushProviderType getType() {
+    return PushProviderType.HMS;
+  }
+
+  @Override
+  public void unregister() {
+    // no implementation
+  }
+
+  @Override
+  public void updateRegistrationId() {
+    Context context = Leanplum.getContext();
+    if (context == null) {
+      return;
+    }
+
+    String regId = null;
+    try {
+      // Read from agconnect-services.json
+      String appId = new AGConnectOptionsBuilder().build(context).getString("client/app_id");
+      regId = HmsInstanceId.getInstance(context).getToken(appId, "HCM");
+    } catch (Throwable e) {
+      Log.e("HMS getToken failed:\n" + Log.getStackTraceString(e));
+    }
+
+    if (!TextUtils.isEmpty(regId)) {
+      setRegistrationId(regId);
+    }
+  }
+}
