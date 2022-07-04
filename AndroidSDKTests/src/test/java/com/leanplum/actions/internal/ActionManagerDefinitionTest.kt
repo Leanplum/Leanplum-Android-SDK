54d2bff308930f01ba8d8dacb60fa52b8f383ef4
Hristo Borisov <hristo.borisov@leanplum.com>
2022-07-04 19:55:57 +0300

    Add tests for queue and definitions

---
+++ AndroidSDKTests/src/test/java/com/leanplum/actions/internal/ActionManagerDefinitionTest.kt
@@ -0,0 +1,76 @@
+package com.leanplum.actions.internal
+
+import android.content.Context
+import com.leanplum.ActionArgs
+import com.leanplum.ActionContext
+import com.leanplum.Leanplum
+import com.leanplum.__setup.AbstractTest
+import com.leanplum.callbacks.ActionCallback
+import com.leanplum.internal.ActionManager
+import com.leanplum.messagetemplates.MessageTemplate
+import com.leanplum.messagetemplates.MessageTemplates
+import org.junit.Assert.*
+import org.junit.Test
+import org.mockito.Mockito
+
+class ActionManagerDefinitionTest : AbstractTest() {
+
+  @Test
+  fun testLeanplumDefine() {
+    val presentHandler = object : ActionCallback() {
+      override fun onResponse(context: ActionContext?): Boolean = true
+    }
+    val dismissHandler = object : ActionCallback() {
+      override fun onResponse(context: ActionContext?): Boolean = true
+    }
+    val name = "action1"
+    val args = ActionArgs()
+
+    Leanplum.defineAction(name, 1, args, presentHandler, dismissHandler)
+
+    val ad = ActionManager.getInstance().definitions.findDefinition(name)
+
+    assertEquals(ad?.name, name)
+    assertEquals(ad?.kind, 1)
+    assertEquals(ad?.args, args)
+    assertEquals(ad?.presentHandler, presentHandler)
+    assertEquals(ad?.dismissHandler, dismissHandler)
+  }
+
+  @Test
+  fun testRegisterMessageTemplate() {
+    val name = "action2"
+    val args = ActionArgs()
+    var presentHandlerInvoked = false
+    var dismissHandlerInvoked = false
+    val actionContext = Mockito.mock(ActionContext::class.java)
+
+    val messageTemplate = object : MessageTemplate {
+      override fun getName(): String = name
+      override fun createActionArgs(context: Context): ActionArgs = args
+      override fun present(context: ActionContext): Boolean {
+        presentHandlerInvoked = true
+        return true
+      }
+      override fun dismiss(context: ActionContext): Boolean {
+        dismissHandlerInvoked = true
+        return true
+      }
+    }
+
+    MessageTemplates.registerTemplate(messageTemplate, mContext)
+
+    val actionDefinition = ActionManager.getInstance().definitions.findDefinition(name)
+
+    assertEquals(actionDefinition?.name, name)
+    assertEquals(actionDefinition?.kind, 3)
+    assertEquals(actionDefinition?.args, args)
+
+    actionDefinition?.presentHandler?.onResponse(actionContext)
+    assertTrue(presentHandlerInvoked)
+
+    actionDefinition?.dismissHandler?.onResponse(actionContext)
+    assertTrue(dismissHandlerInvoked)
+  }
+
+}
