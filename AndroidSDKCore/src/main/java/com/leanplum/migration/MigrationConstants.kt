9ef33f078f8a4c7f601aa7e0772188b585bac503
Hristo Borisov <hristo.borisov@leanplum.com>
2022-10-11 09:55:27 +0300

    Bump CT SDK version to fix logger and UTM Visited (#516)

--- AndroidSDKCore/src/main/java/com/leanplum/migration/MigrationConstants.kt
+++ AndroidSDKCore/src/main/java/com/leanplum/migration/MigrationConstants.kt
@@ -1,9 +1,11 @@
 package com.leanplum.migration
 
+import com.clevertap.android.sdk.CleverTapAPI
+import com.leanplum.internal.Log
+
 object MigrationConstants {
   const val IDENTITY = "Identity"
   const val STATE_PREFIX = "state_"
-  const val UTM_VISITED = "UTM Visited"
 
   const val CHARGED_EVENT_PARAM = "event"
   const val VALUE_PARAM = "value"
@@ -12,4 +14,12 @@ object MigrationConstants {
   const val GP_PURCHASE_DATA_PARAM = "googlePlayPurchaseData"
   const val GP_PURCHASE_DATA_SIGNATURE_PARAM = "googlePlayPurchaseDataSignature"
   const val IAP_ITEM_PARAM = "item"
+
+  fun mapLogLevel(lpLevel: Int): CleverTapAPI.LogLevel = when (lpLevel) {
+    Log.Level.OFF -> CleverTapAPI.LogLevel.OFF
+    Log.Level.ERROR -> CleverTapAPI.LogLevel.INFO
+    Log.Level.INFO -> CleverTapAPI.LogLevel.INFO
+    Log.Level.DEBUG -> CleverTapAPI.LogLevel.VERBOSE
+    else -> CleverTapAPI.LogLevel.INFO
+  }
 }
