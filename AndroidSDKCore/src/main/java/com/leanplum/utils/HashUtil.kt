645ea41b9e9e8c63e9a9d5f4ce449dfceb5da3c2
Hristo Borisov <hristo.borisov@leanplum.com>
2022-10-25 17:44:50 +0300

    Validate and Hash CTID (#522)
    * Hash invalid deviceId and userId before creating CT ID
    
    * Use 50 symbols for deviceId hash; fix lp_devices property
    
    * Set lp_device property for anonymous profiles

---
+++ AndroidSDKCore/src/main/java/com/leanplum/utils/HashUtil.kt
@@ -0,0 +1,31 @@
+package com.leanplum.utils
+
+import java.security.MessageDigest
+
+object HashUtil {
+
+  private fun ByteArray.toHex(limit: Int): String =
+    joinToString(separator = "", limit = limit, truncated = "") { eachByte ->
+      "%02x".format(eachByte)
+    }
+
+  /**
+   * Returns hexadecimal representation of sha256 on the input string.
+   */
+  fun sha256(text: String, limit: Int = 32): String {
+    return MessageDigest
+      .getInstance("SHA-256")
+      .digest(text.toByteArray(Charsets.UTF_8))
+      .toHex(limit)
+  }
+
+  /**
+   * Get first 10 symbols from hexadecimal representation of sha256.
+   */
+  fun sha256_40(text: String) = sha256(text, 5)
+
+  /**
+   * Get first 50 symbols from hexadecimal representation of sha256.
+   */
+  fun sha256_200(text: String) = sha256(text, 25)
+}
