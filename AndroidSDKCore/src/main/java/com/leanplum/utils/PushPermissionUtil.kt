db9ca23b239a308f6907e34805886df1caa60295
Hristo Borisov <hristo.borisov@leanplum.com>
2023-04-07 10:01:04 +0300

    Enable user for onRequestPermissionResult feedback (#544)

--- AndroidSDKCore/src/main/java/com/leanplum/utils/PushPermissionUtil.kt
+++ AndroidSDKCore/src/main/java/com/leanplum/utils/PushPermissionUtil.kt
@@ -24,17 +24,61 @@ package com.leanplum.utils
 import android.Manifest
 import android.annotation.TargetApi
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
+import com.leanplum.Leanplum
 import com.leanplum.internal.Log
+
+private const val DECLINE_LIMIT = 2
 
 /**
  * Could be changed by client if code is already in use. Request code can be used in activity's
- * onRequestPermissionsResult method to receive feedback whether the permission was granted or not.
+ * [onRequestPermissionResult] method to receive feedback whether the permission was granted or not.
  */
 var pushPermissionRequestCode = 1233321
+
+/**
+ * Decline count is tracked only when [onRequestPermissionResult] is invoked by client.
+ *
+ * Setting value of 2 would disable asking for push permission permanently.
+ * Setting value of 0 would ask for push permission again.
+ */
+var declineCount: Int by IntPreference(key = "push_permission_decline_count", defaultValue = 0)
+
+/**
+ * Invoke method from your activity's onRequestPermissionResult to allow Leanplum SDK to track the
+ * number of consecutive declines of the POST_NOTIFICATIONS permission. When two consecutive
+ * declines happen none of the permission dialogs will be shown again and user would have to
+ * manually allow notifications. Note that dismissing the native dialog without clicking on Allow or
+ * Don't Allow does count as a decline from OS API.
+ */
+fun onRequestPermissionResult(requestCode: Int,
+                              permissions: Array<String>,
+                              grantResults: IntArray
+) {
+  val context: Context? = Leanplum.getContext()
+  if (context == null || !BuildUtil.isPushPermissionSupported(context)) {
+    return
+  }
+
+  if (requestCode != pushPermissionRequestCode || permissions.size != grantResults.size) {
+    return
+  }
+
+  for (i in permissions.indices) {
+    if (permissions[i] == Manifest.permission.POST_NOTIFICATIONS) {
+      if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+        declineCount = 0
+      } else {
+        declineCount++
+      }
+      break
+    }
+  }
+}
 
 @TargetApi(33)
 private fun isNotificationPermissionGranted(activity: Activity): Boolean {
@@ -53,13 +97,17 @@ fun shouldShowRegisterForPush(activity: Activity): Boolean {
 fun shouldShowPrePermission(activity: Activity): Boolean {
   return shouldShowRegisterForPush(activity)
       && ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.POST_NOTIFICATIONS)
+      && declineCount < DECLINE_LIMIT
 }
 
 @TargetApi(33)
 fun requestNativePermission(activity: Activity) {
-  activity.requestPermissions(
-    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
-    pushPermissionRequestCode)
+  if (declineCount < DECLINE_LIMIT) {
+    activity.requestPermissions(
+      arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+      pushPermissionRequestCode
+    )
+  }
 }
 
 fun printDebugLog(activity: Activity) {
@@ -73,7 +121,8 @@ fun printDebugLog(activity: Activity) {
 
     Log.d("Notification permission: granted=$permissionGranted " +
         "notificationsEnabled=$notificationsEnabled " +
-        "shouldShowRequestPermissionRationale=$shouldShowRequestPermissionRationale")
+        "shouldShowRequestPermissionRationale=$shouldShowRequestPermissionRationale " +
+        "declineCount=$declineCount")
   } else {
     Log.d("Notification permission: not supported by target or device version")
   }
